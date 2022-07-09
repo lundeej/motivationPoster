fcb52ad4feacf104e4f3e4990f37cd7b4106a6d9
Jesenia Lundeen <jeseniathenotary@gmail.com>
2022-07-09 10:30:20 -0700

    displaying quote in HTML

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+var quoteDiv = document.querySelector("#quote"); 
+
 // Get Kanye API 
-
-function getApit(){
-    var requestQuote = 'https://kanye.rest/'
-
-    fetch(requestQuote, {
-        mode: "cors",
-        headers: {
-            'Access-Control-Allow-Origin': "*"
-        }
-    })
-        .then(function (response) {
-            return response.json(); 
-        })
-        .then(function (data) {
-            console.log(data)
-        }); 
-}
-getApit(); 
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.kanye.rest/';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data.quote)
+        quoteDiv.textContent = data.quote
+      });
+  }
+  getApi();
