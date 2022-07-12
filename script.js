b72bc54abca03d23692c2310fba1bc67e53237b5
colinplancich <colin.plancich@gmail.com>
2022-07-11 17:59:46 -0700

    Pulling repo Monday July 11

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("linked lol");
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.kanye.rest/';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+    
+      });
+  }
+
+  getApi();
+
