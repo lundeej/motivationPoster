1429981279b1412b3ed44b926e3ddbbdb96447ac
colinplancich <colin.plancich@gmail.com>
2022-07-11 20:44:34 -0700

    Modified script.js to display correct image size

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-var quoteDiv = document.querySelector("#quote"); 
-var imageDiv = document.querySelector("#image"); 
+var quoteDiv = document.querySelector("#quote");
+var imageDiv = document.querySelector("#image");
 
 // Get Kanye API 
 function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://api.kanye.rest/';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data.quote)
-        quoteDiv.textContent = data.quote
-      });
-  }
-  getApi();
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = 'https://api.kanye.rest/';
 
-  function getImage() {
-    var requestUrl = 'https://picsum.photos/200/300'; 
-
-    fetch(requestUrl)
+  fetch(requestUrl)
     .then(function (response) {
-      console.log(response.url); 
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.quote)
+      quoteDiv.textContent = data.quote
+    });
+}
+getApi();
+
+function getImage() {
+  var requestUrl = 'https://picsum.photos/600/450';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response.url);
       return response.blob();
     })
-    .then(function (data){
-      console.log(data); 
+    .then(function (data) {
+      console.log(data);
 
-      imageDiv.src = URL.createObjectURL(data); 
+      imageDiv.src = URL.createObjectURL(data);
 
-    }); 
-  }
-  getImage(); 
+    });
+}
+getImage(); 
