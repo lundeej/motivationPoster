965dc8ae9a2ebd26dad06bc109e0f151e50b734e
colinplancich <colin.plancich@gmail.com>
2022-07-11 19:18:42 -0700

    Styled vanilla CSS render of motivational poster

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+var quoteDiv = document.querySelector("#quote");
+var imageDiv = document.querySelector("#image");
+
+// Get Kanye API 
 function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://api.kanye.rest/';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-    
-      });
-  }
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = 'https://api.kanye.rest/';
 
-  getApi();
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.quote)
+      quoteDiv.textContent = data.quote
+    });
+}
+getApi();
 
+function getImage() {
+  var requestUrl = 'https://picsum.photos/200/300';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response.url);
+      return response.blob();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      imageDiv.src = URL.createObjectURL(data);
+
+    });
+}
+getImage(); 
